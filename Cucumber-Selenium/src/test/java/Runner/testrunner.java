5ecbd25f795838b9dd099853a7d18eb5bdab7b49
rahumada <ricardo@enmotionvalue.com>
2019-06-04 19:17:37 +0200

    quality examples

---
+++ Cucumber-Selenium/src/test/java/Runner/testrunner.java
@@ -0,0 +1,28 @@
+package Runner;
+
+import java.io.File;
+
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+import com.cucumber.listener.Reporter;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+features = "src/test/java/Features"
+,glue= {"seleniumgluecode"},
+//plugin = { "pretty", "html:target/htmlreports" },
+plugin = { "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}, 
+monochrome = true
+)
+
+public class testrunner {
+	@AfterClass
+    public static void writeExtentReport() {
+        Reporter.loadXMLConfig(new File("config/report.xml"));
+    
+    }
+}
